9fb6cf4442bfdbc19a52b7521ff30b97584f62d9
lateo <lateo@pop-os.localdomain>
2020-04-20 22:48:21 +0000

    trying first deployment

--- server/app.js
+++ server/app.js
@@ -3,11 +3,16 @@ const graphqlHttp = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 
+app.use('/', serveStatic(path.join('../frontend', '/dist')))
+
+const port = process.env.PORT || 8080
 // connecting to database
 mongoose.connect('mongodb+srv://lateo:killme2016@frontend-5wbny.mongodb.net/test?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
@@ -19,6 +24,7 @@ app.use('/graphql', graphqlHttp({
 	graphiql: true
 }));
 
-app.listen('4000', () => {
-	console.log('Listening on port 4000');
-})
+// app.listen('4000', () => {
+// 	console.log('Listening on port 4000');
+// })
+app.listen(port);
